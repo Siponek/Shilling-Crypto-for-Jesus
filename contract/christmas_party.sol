66c929885156d7116254bd92871d766404bdc946
siponek <szinkowicz@gmail.com>
2024-05-16 20:21:10 +0200

    feat: optimizing for gas

--- contract/christmas_party.sol
+++ contract/christmas_party.sol
@@ -7,9 +7,9 @@ contract ChristmasLottery {
 
     event Response(bool success, bytes data);
     event LotteryReset();
+
     address public owner;
     mapping(address => bool) public authorized;
-    mapping(address => uint8[]) private ticketsByOwner;
     mapping(uint8 => bool) private isWinningTicket;
     uint8 public totalTicketsInGame;
     // actual tickets that yet to be arrayWinningTicketsId
@@ -19,11 +19,16 @@ contract ChristmasLottery {
         string firstName;
         string lastName;
         string studentId;
-        address walletAddress;
+        
     }
-    
+    struct TicketRange {
+        uint8 startTicket;
+        uint8 count;
+    }
+
+    mapping(address => TicketRange[]) private ticketsByOwnerRange;
     mapping(uint8 => Participant) public ticketOwner;
-    uint8[] public arrayWinningTicketsId;
+    uint8[] private arrayWinningTicketsId;
 
     constructor() {
         owner = msg.sender;
@@ -60,13 +65,12 @@ contract ChristmasLottery {
         authorized[_addr] = false;
     }
 
-    // Modified to accept participant details
     function buyTickets(uint8 numberOfTickets, string memory firstName, string memory lastName, string memory studentId)
     external
     // payable
-    // Allowing everyone to buy tickets does not make sense without paying
-    // So owner can authorize addresses to buy tickets as his dealers
-    onlyAuthorized
+    // Owner could authorize addresses to buy tickets as his dealers
+    // onlyAuthorized
+    onlyOwner
     personExists(firstName,
     lastName,
     studentId)
@@ -76,52 +80,68 @@ contract ChristmasLottery {
     {
         require(numberOfTickets > 0 && numberOfTickets <= MAX_TICKETS_PER_PURCHASE, "Ticket purchase exceeds limit");
 
+        // Create a new participant struct
+        Participant memory participant = Participant(firstName, lastName, studentId);
+        
+        // Calculate the start ticket and the end ticket
+        uint8 startTicket = totalTicketsInGame;
+        totalTicketsInGame += numberOfTickets; // Increment the total tickets in the game
+
+        // Record the range of tickets bought
+        TicketRange memory range = TicketRange(startTicket, numberOfTickets);
+        ticketsByOwnerRange[msg.sender].push(range);
+
+        // Assign all tickets within the range to the buyer
         for (uint8 i = 0; i < numberOfTickets; i++) {
-            Participant memory participant = Participant(firstName, lastName, studentId, msg.sender);
-            uint8 ticketId = totalTicketsInGame++;
-            tickets.push(ticketId);
-            ticketOwner[ticketId] = participant;
-            ticketsByOwner[msg.sender].push(ticketId);
+            // You might want to rethink if this mapping is still necessary
+            ticketOwner[startTicket + i] = participant;
         }
     }
 
-    function drawWinners(uint8 numberOfWinners) external onlyAuthorized {
+    function drawWinners2(uint8 numberOfWinners) external onlyOwner returns (uint8[] memory) {
         require(numberOfWinners <= tickets.length, "More winners than tickets available");
-
+        uint8[] memory winners = new uint8[](numberOfWinners);
         for (uint8 i = 0; i < numberOfWinners; i++) {
-            uint8 winnerIndex = uint8(uint(random() % tickets.length));
+            uint8 winnerIndex;
+            bool isUnique;
+            do {
+                winnerIndex = uint8(random() % tickets.length);
+                isUnique = !isWinningTicket[tickets[winnerIndex]];
+            } while (!isUnique);
+
             uint8 winnerTicket = tickets[winnerIndex];
-            arrayWinningTicketsId.push(winnerTicket);
+            winners[i] = winnerTicket;
             isWinningTicket[winnerTicket] = true; // Mark as winner
-            // Remove the selected ticket from the pool to avoid duplicates
-            // Assign the last ticket in place for the winning one
-            // Then pop it
-            tickets[winnerIndex] = tickets[tickets.length - 1];
-            tickets.pop();
+            arrayWinningTicketsId.push(winnerTicket);
         }
+        return arrayWinningTicketsId;
     }
 
-    // Updated to return participant details
+
+    // function drawWinners(uint8 numberOfWinners) external onlyOwner returns (uint8[] memory){
+    //     require(numberOfWinners <= tickets.length, "More winners than tickets available");
+
+    //     for (uint8 i = 0; i < numberOfWinners; i++) {
+    //         uint8 winnerIndex = uint8(uint(random() % tickets.length));
+    //         uint8 winnerTicket = tickets[winnerIndex];
+    //         arrayWinningTicketsId.push(winnerTicket);
+    //         isWinningTicket[winnerTicket] = true; // Mark as winner
+    //         // Remove the selected ticket from the pool to avoid duplicates
+    //         // Assign the last ticket in place for the winning one
+    //         // Then pop it
+    //         tickets[winnerIndex] = tickets[tickets.length - 1];
+    //         tickets.pop();
+    //     }
+    //     return arrayWinningTicketsId;
+    // }
+
     function getWinnerDetails(uint8 ticket) external view onlyAuthorized returns (string memory, string memory, string memory) {
         Participant memory winner = ticketOwner[ticket];
         return (winner.firstName, winner.lastName, winner.studentId);
     }
 
-    function checkThisAccWinningTickets() public view returns (uint8[] memory) {
-        uint8[] memory myTickets = ticketsByOwner[msg.sender];
-        uint8[] memory myWinners = new uint8[](myTickets.length);
-        uint8 winnerCount = 0;
-        for (uint8 i = 0; i < myTickets.length; i++) {
-            if (isWinner(myTickets[i])) {
-                myWinners[winnerCount++] = myTickets[i];
-            }
-        }
-        // Resize the array to only include found arrayWinningTicketsId
-        uint8[] memory finalWinners = new uint8[](winnerCount);
-        for (uint8 j = 0; j < winnerCount; j++) {
-            finalWinners[j] = myWinners[j];
-        }
-        return finalWinners;
+    function getAllCurrentlyWinningTickets() public view returns (uint8[] memory) {
+        return arrayWinningTicketsId;
     }
 
     function resetLottery() external onlyOwner {
@@ -134,21 +154,19 @@ contract ChristmasLottery {
         // Reset each participant's list of tickets and the ticket ownership mapping
         for (uint8 i = 0; i < totalTicketsInGame; i++) {
             uint8 ticketId = tickets[i];
-            delete ticketsByOwner[ticketOwner[ticketId].walletAddress];
             delete ticketOwner[ticketId];
         }
-
-        // Clear the array of winners
-        delete arrayWinningTicketsId;
 
         // Reset the isWinningTicket mapping for all ticket IDs
         for (uint8 j = 0; j < arrayWinningTicketsId.length; j++) {
             delete isWinningTicket[arrayWinningTicketsId[j]];
         }
+
+        // Clear the array of winners
+        delete arrayWinningTicketsId;
         delete totalTicketsInGame;
         emit LotteryReset();
     }
-
 
     function random() private view returns (uint8) {
         bytes32 hashValue = keccak256(abi.encodePacked(block.prevrandao, block.timestamp, tickets));
@@ -164,7 +182,7 @@ contract ChristmasLottery {
 
     }
 
-    function isWinner(uint8 ticketNumber) public view returns (bool) {
+    function isWinner(uint8 ticketNumber) private view returns (bool) {
         return isWinningTicket[ticketNumber];
     }
 }
