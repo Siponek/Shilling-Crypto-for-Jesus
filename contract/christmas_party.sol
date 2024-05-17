66d9541f4de12ed8bbbfa614b2d44b5fac67fe03
siponek <szinkowicz@gmail.com>
2024-05-17 16:16:32 +0200

    feat: finished normal contract

--- contract/christmas_party.sol
+++ contract/christmas_party.sol
@@ -2,6 +2,24 @@
 pragma solidity ^0.8.24;
 
 contract ChristmasLottery {
+        // -----GOTTA GO FAST
+        // Implement binary search for adding ranges of numbers
+    struct Range{
+        uint8 start;
+        uint8 end;
+    }
+    struct Participant_fast {
+        string firstName;
+        string lastName;
+    }
+    // For keepking track of existing participants
+    mapping(string => bool) exisiting_participants;
+    // For keeping track of ranges of numbers
+    mapping (string => Range) participants_ranges;
+    // For keeping track of participants data
+    mapping (uint256 => Participant_fast) participants_list;
+        // -----GOTTA GO FAST
+
     // Example limit
     uint8 public constant MAX_TICKETS_PER_PURCHASE = 10;
 
@@ -11,6 +29,7 @@ contract ChristmasLottery {
     address public owner;
     mapping(address => bool) public authorized;
     mapping(uint8 => bool) private isWinningTicket;
+
     uint8 public totalTicketsInGame;
     // actual tickets that yet to be arrayWinningTicketsId
     uint8[] private tickets;
@@ -21,14 +40,10 @@ contract ChristmasLottery {
         string studentId;
         
     }
-    struct TicketRange {
-        uint8 startTicket;
-        uint8 count;
-    }
+    mapping(address => uint8[]) private ticketsByOwner;
 
-    mapping(address => TicketRange[]) private ticketsByOwnerRange;
     mapping(uint8 => Participant) public ticketOwner;
-    uint8[] private arrayWinningTicketsId;
+    uint8[] public arrayWinningTicketsId;
 
     constructor() {
         owner = msg.sender;
@@ -36,6 +51,7 @@ contract ChristmasLottery {
         authorized[owner] = true;
     }
 
+    // ----------------
     modifier onlyOwner() {
         require(msg.sender == owner, "Not authorized: owner required");
         _;
@@ -56,6 +72,10 @@ contract ChristmasLottery {
         require(bytes(str).length <= maxLength, "String exceeds maximum length");
         _;
     }
+    // ----------------
+
+
+
 
     function authorizeAddress(address _addr) external onlyOwner {
         authorized[_addr] = true;
@@ -65,84 +85,72 @@ contract ChristmasLottery {
         authorized[_addr] = false;
     }
 
-    function buyTickets(uint8 numberOfTickets, string memory firstName, string memory lastName, string memory studentId)
-    external
-    // payable
-    // Owner could authorize addresses to buy tickets as his dealers
-    // onlyAuthorized
-    onlyOwner
-    personExists(firstName,
-    lastName,
-    studentId)
-    validLength(firstName,13)
-    validLength(lastName,13)
-    validLength(studentId,13)
+    function buyTicketsOffchainMode(uint8 numberOfTickets, string memory firstName, string memory lastName, string memory studentId)
+        external
+        // payable
+        // Owner can authorize addresses to buy tickets as his dealers
+        onlyOwner
+        personExists(firstName,
+        lastName,
+        studentId)
+        validLength(firstName,13)
+        validLength(lastName,13)
+        validLength(studentId,13)
     {
         require(numberOfTickets > 0 && numberOfTickets <= MAX_TICKETS_PER_PURCHASE, "Ticket purchase exceeds limit");
-
-        // Create a new participant struct
         Participant memory participant = Participant(firstName, lastName, studentId);
-        
-        // Calculate the start ticket and the end ticket
-        uint8 startTicket = totalTicketsInGame;
-        totalTicketsInGame += numberOfTickets; // Increment the total tickets in the game
+        exisiting_participants[studentId] = true;
+        participants_ranges;
+        // participants_list
+    }
 
-        // Record the range of tickets bought
-        TicketRange memory range = TicketRange(startTicket, numberOfTickets);
-        ticketsByOwnerRange[msg.sender].push(range);
-
-        // Assign all tickets within the range to the buyer
+    function buyTickets(uint8 numberOfTickets, string memory firstName, string memory lastName, string memory studentId)
+        external
+        // payable
+        // Owner can authorize addresses to buy tickets as his dealers
+        onlyOwner
+        personExists(firstName,
+        lastName,
+        studentId)
+        validLength(firstName,13)
+        validLength(lastName,13)
+        validLength(studentId,13)
+    {
+        require(numberOfTickets > 0 && numberOfTickets <= MAX_TICKETS_PER_PURCHASE, "Ticket purchase exceeds limit");
         for (uint8 i = 0; i < numberOfTickets; i++) {
-            // You might want to rethink if this mapping is still necessary
-            ticketOwner[startTicket + i] = participant;
+            Participant memory participant = Participant(firstName, lastName, studentId);
+            uint8 ticketId = totalTicketsInGame++;
+            tickets.push(ticketId);
+            ticketOwner[ticketId] = participant;
+            ticketsByOwner[msg.sender].push(ticketId);
         }
     }
 
-    function drawWinners2(uint8 numberOfWinners) external onlyOwner returns (uint8[] memory) {
+    function drawWinners(uint8 numberOfWinners) external onlyOwner returns (uint8[] memory){
         require(numberOfWinners <= tickets.length, "More winners than tickets available");
-        uint8[] memory winners = new uint8[](numberOfWinners);
-        for (uint8 i = 0; i < numberOfWinners; i++) {
-            uint8 winnerIndex;
-            bool isUnique;
-            do {
-                winnerIndex = uint8(random() % tickets.length);
-                isUnique = !isWinningTicket[tickets[winnerIndex]];
-            } while (!isUnique);
 
+        for (uint8 i = 0; i < numberOfWinners; i++) {
+            uint8 winnerIndex = uint8(uint(random() % tickets.length));
             uint8 winnerTicket = tickets[winnerIndex];
-            winners[i] = winnerTicket;
-            isWinningTicket[winnerTicket] = true; // Mark as winner
             arrayWinningTicketsId.push(winnerTicket);
+            isWinningTicket[winnerTicket] = true; // Mark as winner
+            // Remove the selected ticket from the pool to avoid duplicates
+            // Assign the last ticket in place for the winning one
+            // Then pop it
+            tickets[winnerIndex] = tickets[tickets.length - 1];
+            tickets.pop();
         }
         return arrayWinningTicketsId;
     }
-
-
-    // function drawWinners(uint8 numberOfWinners) external onlyOwner returns (uint8[] memory){
-    //     require(numberOfWinners <= tickets.length, "More winners than tickets available");
-
-    //     for (uint8 i = 0; i < numberOfWinners; i++) {
-    //         uint8 winnerIndex = uint8(uint(random() % tickets.length));
-    //         uint8 winnerTicket = tickets[winnerIndex];
-    //         arrayWinningTicketsId.push(winnerTicket);
-    //         isWinningTicket[winnerTicket] = true; // Mark as winner
-    //         // Remove the selected ticket from the pool to avoid duplicates
-    //         // Assign the last ticket in place for the winning one
-    //         // Then pop it
-    //         tickets[winnerIndex] = tickets[tickets.length - 1];
-    //         tickets.pop();
-    //     }
-    //     return arrayWinningTicketsId;
-    // }
 
     function getWinnerDetails(uint8 ticket) external view onlyAuthorized returns (string memory, string memory, string memory) {
         Participant memory winner = ticketOwner[ticket];
         return (winner.firstName, winner.lastName, winner.studentId);
     }
 
-    function getAllCurrentlyWinningTickets() public view returns (uint8[] memory) {
-        return arrayWinningTicketsId;
-    }
+    // function getAllCurrentlyWinningTickets() public view returns (uint8[] memory) {
+    //     return arrayWinningTicketsId;
+    // }
 
     function resetLottery() external onlyOwner {
         // Reset total tickets in game
@@ -185,4 +193,64 @@ contract ChristmasLottery {
     function isWinner(uint8 ticketNumber) private view returns (bool) {
         return isWinningTicket[ticketNumber];
     }
+/*
+    function drawWinners2(uint8 numberOfWinners) external onlyOwner returns (uint8[] memory) {
+        require(numberOfWinners <= tickets.length, "More winners than tickets available");
+        uint8[] memory winners = new uint8[](numberOfWinners);
+        for (uint8 i = 0; i < numberOfWinners; i++) {
+            uint8 winnerIndex;
+            bool isUnique;
+            do {
+                winnerIndex = uint8(random() % tickets.length);
+                isUnique = !isWinningTicket[tickets[winnerIndex]];
+            } while (!isUnique);
+
+            uint8 winnerTicket = tickets[winnerIndex];
+            winners[i] = winnerTicket;
+            isWinningTicket[winnerTicket] = true; // Mark as winner
+            arrayWinningTicketsId.push(winnerTicket);
+        }
+        return arrayWinningTicketsId;
+    }
+*/
+    function addParticipant(string memory _studentID) private returns (Participant) {
+        // if the participant is not already in the list of participants
+        if (!Participant_fast[_studentID]) {
+            // add the participant to the mapping of participants with the studentID as key
+            Participant_fast[_studentID] = true;
+        }
+        Participant_fast[_studentID];
+    }
+
+    function addRange(uint8 _start, uint8 _end) public {
+        require(_start <= _end, "Invalid range: start must be less than or equal to end");
+
+        if (ranges.length > 0) {
+            Range storage lastRange = ranges[ranges.length - 1];
+            require(_start > lastRange.end, "Ranges must be non-overlapping and in order");
+        }
+
+        ranges.push(Range(_start, _end));
+    }
+
+    function findRange(uint8 number) public view returns (uint8) {
+        int256 left = 0;
+        int256 right = int256(ranges.length) - 1;
+
+        while (left <= right) {
+            int256 mid = (left + right) / 2;
+            Range storage range = ranges[uint256(mid)];
+
+            if (number >= range.start && number <= range.end) {
+                return mid;
+            } else if (number < range.start) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1; // Return -1 if no range contains the number
+    }
+
 }
