ddf468c7cf0dd6f17524a7e9b02b4b7fad94cf8a
siponek <szinkowicz@gmail.com>
2024-05-26 14:24:09 +0200

    feat: Fixed main layout of app

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
 </script>
 
 <template>
-    <v-app class="rounded rounded-md bg-yellow" full-height>
+    <v-app
+        class="rounded rounded-md"
+        style="background: lightblue"
+        full-height
+    >
         <v-app-bar clipped-right
             ><v-app-bar-title
                 >Christmass Lottery Contract
@@ -12,39 +16,23 @@
         >
         <SideBar />
 
-        <v-main
-            style="background: lightcoral"
-            class="bg-green"
-        >
-            <v-container style="background: lightblue">
+        <v-main>
+            <v-container>
                 <router-view />
             </v-container>
         </v-main>
         <v-footer
             class="bg-indigo-lighten-1 text-center d-flex flex-column"
         >
-            <div></div>
-
             <div class="pt-0">
-                Phasellus feugiat arcu sapien, et iaculis ipsum
-                elementum sit amet. Mauris cursus commodo
-                interdum. Praesent ut risus eget metus luctus
-                accumsan id ultrices nunc. Sed at orci sed
-                massa consectetur dignissim a sit amet dui.
-                Duis commodo vitae velit et faucibus. Morbi
-                vehicula lacinia malesuada. Nulla placerat
-                augue vel ipsum ultrices, cursus iaculis dui
-                sollicitudin. Vestibulum eu ipsum vel diam
-                elementum tempor vel ut orci. Orci varius
-                natoque penatibus et magnis dis parturient
-                montes, nascetur ridiculus mus.
+                Lorem ipsum dolor sit amet consectetur
             </div>
 
             <v-divider></v-divider>
 
             <div>
                 {{ new Date().getFullYear() }} —
-                <strong>Vuetify</strong>
+                <strong>Szymon Zinkowicz</strong>
             </div>
         </v-footer>
     </v-app>
