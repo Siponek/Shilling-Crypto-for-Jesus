891e45b812f3d82a8b73c834ba0839a16548ddfd
siponek <szinkowicz@gmail.com>
2024-07-22 19:51:47 +0200

    ref: merged vitest and vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+/// <reference types="vitest" />
 import { fileURLToPath, URL } from 'node:url'
-
+import { configDefaults } from 'vitest/config'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -7,14 +8,17 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), vueJsx(), VueDevTools()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(
+                new URL('./src', import.meta.url)
+            )
+        }
+    },
+    test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url))
     }
-  }
 })
